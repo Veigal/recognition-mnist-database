48ea44b685b58d40f4c1e628cdfe5fa32848ed11
Veigal <53204081+Veigal@users.noreply.github.com>
2019-07-23 18:15:59 -0300

    Update routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,16 +2,14 @@ const routes = require("express").Router();
 const multer = require("multer");
 const multerConfig = require("./config/multer");
 
-var request = require("request");
 var fs = require("fs");
 
 
 var unirest = require("unirest");
 
-var reque = unirest("POST", "https://eastus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/5a09f609-1d9e-4d92-bb22-21a35261418f/classify/iterations/0.0/image");
+var reque = unirest("POST", "https://eastus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/5a09f609-1d9e-4d92-bb22-21a35261418f/classify/iterations/Iteration2/image");
 
 
-//todo: no post acho que não precisa retornar nada
 routes.post("/posts", multer(multerConfig).single("file"), (req, res) => {
 
   var prob=0, value, aux=0;
@@ -29,24 +27,14 @@ routes.post("/posts", multer(multerConfig).single("file"), (req, res) => {
   
   reque.end(function (resp) {
     if (resp.error) throw new Error(resp.error);
-  
-    console.log(resp.body["iteration"]);
-
-
-
     for(var attributename in resp.body["predictions"]){
       aux=resp.body["predictions"][attributename]["probability"],
-      aux=parseFloat(aux),
-      console.log('1---' + aux)
-      console.log('2---' + resp.body["predictions"][attributename]["tagName"])
+      aux=parseFloat(aux)
       if (prob<aux){
         prob=aux,
         value=resp.body["predictions"][attributename]["tagName"]
-        console.log('3---' + value)
       };
     }
-
-    console.log('4---' + value);
     return res.json({number : value});
   });
 });
@@ -57,5 +45,3 @@ routes.post("/posts", multer(multerConfig).single("file"), (req, res) => {
 
 
 module.exports = routes;
-
-
